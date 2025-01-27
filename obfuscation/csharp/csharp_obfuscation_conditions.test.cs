8c810728046cfe9de0be37a85e1f7a0001893c4f
Matan-Giladi <136958960+Matan-Giladi@users.noreply.github.com>
2025-01-27 04:53:48 +0200

    Improve obfuscation by operators noise detection and more

--- obfuscation/csharp/csharp_obfuscation_conditions.test.cs
+++ obfuscation/csharp/csharp_obfuscation_conditions.test.cs
@@ -1,3 +1,3 @@
 if ('a') { Console.WriteLine("True"); }
 while (2>1) { Console.WriteLine("False"); }
-switch (true) { case true: Console.WriteLine("True"); break; }
+switch (true) { case 2: Console.WriteLine("True"); break; }
